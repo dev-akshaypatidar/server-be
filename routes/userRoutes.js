9939600cf47d3a8c7b4f46e745d7a44c34376f55
Akshay <edu.akshaypatidar@outlook.com>
2026-05-30 00:16:53 +0530

    Fix formatting by adding a space before the end of file in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,4 +5,4 @@ const userController = require("../controllers/userControllers");
 router.post("/add", userController.addUser);
 router.post("/bulk", userController.addUsersBulk);
 
-module.exports = router;
+module.exports = router; 
